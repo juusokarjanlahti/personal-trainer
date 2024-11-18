d24411fc75bc1e0d3f2a4e406d4fec861e562924
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2024-11-18 09:37:58 +0200

    Updated project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+    <div className="App">
+        <header className="App-header">
+            <h1>React 18</h1>
+            <p>React 18 is here!</p>
+        </header>
+    </div>
+    </>
+  )
+}
+
+export default App
